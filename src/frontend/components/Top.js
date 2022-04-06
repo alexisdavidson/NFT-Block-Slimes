336c9523b870e8850ab384f8db6d844c12e95172
Alexis Davidson <alexisdavidson00@gmail.com>
2022-04-06 21:21:44 +0200

    Add slimes grid

--- src/frontend/components/Top.js
+++ src/frontend/components/Top.js
@@ -1,5 +1,9 @@
 import { Image, Row, Col } from 'react-bootstrap'
-import slimes from './images/slimes.gif'
+import slime from './images/slime.png'
+import slime1 from './images/slimes/1.png'
+import slime2 from './images/slimes/2.png'
+import slime3 from './images/slimes/3.png'
+import slime4 from './images/slimes/4.png'
 import logo from './images/logo.png'
 
 const Top = () => {
@@ -7,7 +11,7 @@ const Top = () => {
         <Row>
             <Row className="mt-5 p-3">
                 <Col className="col-12 col-lg-6">
-                    <img src={slimes} alt="Presentation image" className="rounded" style={{
+                    <img src={slime} alt="Presentation image" className="rounded" style={{
                             border: "20px solid #0d1730"
                         }}/>
                 </Col>
@@ -61,6 +65,30 @@ const Top = () => {
                         <h3>Volume Traded</h3>
                         <h2>0.000 ETH</h2>
                     </div>
+                </Col>
+            </Row>
+            <Row className="p-3" style={{
+                            fontSize: "25px"
+            }}>
+                <Col className="col-12 col-lg-6 col-xl-3 mb-4">
+                    <img src={slime1} alt="Presentation image" className="rounded" style={{
+                            border: "20px solid #0d1730"
+                        }}/>
+                </Col>
+                <Col className="col-12 col-lg-6 col-xl-3 mb-4">
+                    <img src={slime2} alt="Presentation image" className="rounded" style={{
+                            border: "20px solid #0d1730"
+                        }}/>
+                </Col>
+                <Col className="col-12 col-lg-6 col-xl-3 mb-4">
+                    <img src={slime3} alt="Presentation image" className="rounded" style={{
+                            border: "20px solid #0d1730"
+                        }}/>
+                </Col>
+                <Col className="col-12 col-lg-6 col-xl-3 mb-4">
+                    <img src={slime4} alt="Presentation image" className="rounded" style={{
+                            border: "20px solid #0d1730"
+                        }}/>
                 </Col>
             </Row>
         </Row>
